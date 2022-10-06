9f2aa2217b31358bda8e0e116b3499de105a863e
Emerson Silva Higino Filho <emersonfilho1@hotmail.com>
2022-10-06 17:13:12 -0300

    login page

---
+++ src/views/Login.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="login">
+    <h1>Login</h1>
+    <form>
+      <label for="email">email </label>
+      <input type="email" name="email" id="email" v-model="login.email" />
+      <label for="senha">senha </label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        v-model="login.password"
+      />
+      <button class="btn" @click="logar">Logar</button>
+    </form>
+    <p class="perdeu">
+      <a href="/" target="_blank">Esqueceu a senha ? Clique aqui.</a>
+    </p>
+    <LoginCriar></LoginCriar>
+  </section>
+</template>
+
+<script>
+import LoginCriar from "@/components/LoginCriar.vue";
+export default {
+  name: "LoginUser",
+  components: {
+    LoginCriar,
+  },
+  data() {
+    return {
+      login: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    logar() {},
+  },
+};
+</script>
+
+<style scoped>
+.login {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 40px;
+  color: #87f;
+}
+
+form {
+  display: grid;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.perdeu {
+  text-align: center;
+  margin: 20px auto 0 auto;
+}
+.perdeu a:hover {
+  color: #87f;
+  text-decoration: underline;
+}
+</style>
